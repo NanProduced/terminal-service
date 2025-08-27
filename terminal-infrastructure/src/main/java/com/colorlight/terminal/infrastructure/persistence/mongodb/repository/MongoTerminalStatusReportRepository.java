937ccfa8152311a1db054a58bd5959be59592b00
Nan <nan@lednets.com>
2025-08-27 11:30:00 +0800

    Revert "设备详情获取"
    This reverts commit 845c1cbb254f2490d63a4b19ab58e3b966ecdb0e.

--- terminal-infrastructure/src/main/java/com/colorlight/terminal/infrastructure/persistence/mongodb/repository/MongoTerminalStatusReportRepository.java
+++ terminal-infrastructure/src/main/java/com/colorlight/terminal/infrastructure/persistence/mongodb/repository/MongoTerminalStatusReportRepository.java
@@ -11,13 +11,12 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 /**
  * 终端上报数据的MongoDB存储实现类
  *
- * @author Demon
+ * @author Nan
  */
 @Repository
 @RequiredArgsConstructor
@@ -47,27 +46,5 @@ public class MongoTerminalStatusReportRepository implements TerminalStatusReport
             newDoc.setUpdateTime(now);
             mongoTemplate.save(newDoc);
         }
-    }
-
-    @Override
-    public Optional<TerminalStatusReport> findByDeviceId(Long deviceId) {
-        Query query = Query.query(Criteria.where("deviceId").is(deviceId));
-        TerminalStatusReportDocument document = mongoTemplate.findOne(query, TerminalStatusReportDocument.class);
-        return Optional.ofNullable(document)
-                .map(TerminalStatusReportDocument::getTerminalStatusReport);
-    }
-
-    @Override
-    public Map<Long, TerminalStatusReport> findByDeviceIds(List<Long> deviceIds) {
-        Query query = Query.query(Criteria.where("deviceId").in(deviceIds));
-        List<TerminalStatusReportDocument> documents = mongoTemplate.find(query, TerminalStatusReportDocument.class);
-
-        return documents.stream()
-                .filter(doc -> doc.getDeviceId() != null && doc.getTerminalStatusReport() != null)
-                .collect(Collectors.toMap(
-                        TerminalStatusReportDocument::getDeviceId,
-                        TerminalStatusReportDocument::getTerminalStatusReport,
-                        (existing, replacement) -> replacement // 如果有重复key，使用新值
-                ));
     }
 }
